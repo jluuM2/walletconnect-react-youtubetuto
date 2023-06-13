74aafcb2fe21b21ce214d4d47c3b1ff8ecd67fd4
Jose Luu <jose.luu@gmail.com>
2023-06-13 15:27:17 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
+import { SignClient } from "@walletconnect/sign-client";
+import { useEffect, useState } from "react";
+import {Web3Modal } from "@web3modal/standalone"
 import './App.css';
+import { ErrorDescription } from "ethers";
+
+const web3Modal = new Web3Modal({
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  standaloneChains: ["eip155:80001"]
+})
 
 function App() {
+  const [signClient, setSignClient ] = useState();
+  const [sessions, setSessions] = useState([]);
+  const [accounts, setAccounts] = useState([]);
+
+  async function createClient(){
+
+    try{
+      const client = await SignClient.init({
+        projectId: process.env.REACT_APP_PROJECT_ID
+      })
+      console.log('çlient', client);
+      setSignClient(client);
+    } catch(e) {
+      console.log(e);
+    }
+  }
+
+  async function handleConnect() {
+    if (!signClient) throw Error("Cannot connect. signClient has not been initialized");
+    try {
+//proposal namespace
+      const proposalNamespace = {
+        eip155: {
+          chains: ["eip155:80001"],
+          methods: ["eth_send_Transaction"],
+          events: ["connect", "disconnect"]
+        }
+      }
+      const {uri, approval} = await signClient.connect( {
+        requiredNamespaces: proposalNamespace
+      })
+      console.log('uri', uri)
+      if (uri) {
+        web3Modal.openModal({uri})
+        const sessionNamespace = await approval();
+        console.log('sessionnamespace',sessionNamespace )
+        onSessionConnect(sessionNamespace)
+        web3Modal.closeModal()
+      }
+    }catch (e) {
+      console.log(e);
+    }
+  }
+
+  async function onSessionConnect(session){
+    if (!session) throw Error("session not exist")
+    try {
+      setSessions(session)
+      setAccounts(session.namespaces.eip155.accounts[0].slice(9));
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    if (!signClient){
+      createClient();
+    }
+  },[signClient]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Youtube tuto</h1>
+      { accounts.length ? 
+        (<p>{accounts}</p>) :
+          (<button onClick={handleConnect} disabled={!signClient}>
+            Connect
+          </button>)
+      }
     </div>
   );
 }
