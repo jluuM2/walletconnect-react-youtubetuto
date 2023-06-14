b0d968c536c1ff609bf57d423833401f66616c37
Jose Luu <jose.luu@gmail.com>
2023-06-14 13:53:43 +0200

    fin projet: fonctionne

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ function App() {
   const [signClient, setSignClient ] = useState();
   const [sessions, setSessions] = useState([]);
   const [accounts, setAccounts] = useState([]);
+  const [txHash, setTxHash] = useState([]);
 
   async function createClient(){
 
@@ -22,6 +23,7 @@ function App() {
       })
       console.log('çlient', client);
       setSignClient(client);
+      await subscribeToEvents(client);
     } catch(e) {
       console.log(e);
     }
@@ -34,7 +36,7 @@ function App() {
       const proposalNamespace = {
         eip155: {
           chains: ["eip155:80001"],
-          methods: ["eth_send_Transaction"],
+          methods: ["eth_sendTransaction"],
           events: ["connect", "disconnect"]
         }
       }
@@ -58,11 +60,67 @@ function App() {
     if (!session) throw Error("session not exist")
     try {
       setSessions(session)
-      setAccounts(session.namespaces.eip155.accounts[0].slice(9));
+      const eip155AccountComponents = session.namespaces.eip155.accounts[0].split(':')
+      setAccounts(eip155AccountComponents[2]);
     } catch (error) {
       console.log(error)
     }
   }
+
+  async function handleDisconnect(){
+    try {
+      await signClient.disconnect({
+        topic: sessions.topic,
+        code: 6000,
+        message: "User disconnected"
+      });
+      reset();
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  async function subscribeToEvents(client){
+    if (!client) throw Error("No client to subscribe to")
+    try {
+      client.on("session_delete",() => {
+        console.log("user disconnected the session from the wallet");
+        reset()
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  async function handleSend(){
+    try {
+      const tx = {
+        from: accounts,
+        to: "0x50Edb3d07314a4ADf165D62aC04CF0265307FfC6",
+        data: "0x",
+        gasPrice: "0x06FC23AC00",
+        gasLimit: "0xA410",
+        value: "0x38D7EA4C68000" // 1 milli ether
+      };
+      const result = await signClient.request({
+        topic: sessions.topic,
+        request:{
+          method: "eth_sendTransaction",
+          params:  [tx]
+        },
+        chainId: "eip155:80001"
+      })
+      console.log("send Tx result", result);
+      setTxHash(result)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  
+  const reset = () => {
+    setAccounts([]);
+    setSessions([]);
+  };
 
   useEffect(() => {
     if (!signClient){
@@ -73,10 +131,21 @@ function App() {
     <div className="App">
       <h1>Youtube tuto</h1>
       { accounts.length ? 
-        (<p>{accounts}</p>) :
-          (<button onClick={handleConnect} disabled={!signClient}>
+        (<>
+          <p>{accounts}</p>
+          <button onClick={handleDisconnect}>Disconnect</button>
+          <button onClick={handleSend}>SendTx</button>
+          {txHash && <p>View Tx -
+            <a href={`https://mumbai.polygonscan.com/tx/${txHash}`} 
+            target="_blank" rel="noreferrer">here</a>
+            !</p>}
+        </>
+        )
+        : (
+            <button onClick={handleConnect} disabled={!signClient}>
             Connect
-          </button>)
+            </button>
+          )
       }
     </div>
   );
